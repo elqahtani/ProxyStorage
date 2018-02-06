c7c8e6f7494c2019b721bdb2966fc5efcc7540a8
Huda Ridwan <kerupuk.lovers@gmail.com>
2018-02-06 16:35:43 +0700

    Add GET Method for /

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const app = express(); // define our app using express
+
+// const ListProxies = require('./proxy.json');
+
+
+
+// router.get('/api/proxy', function(req, res, next) {
+//     res.send({
+//         message: "ListProxies",
+//         members: proxies
+//     })
+// })
+
+
+router.get('/api', function(req, res) {
+    res.json({ message: 'Welcome to Api Proxy-Storage !' });
+});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Welcome to Api Proxy-Storage' });
 });
 
 module.exports = router;
