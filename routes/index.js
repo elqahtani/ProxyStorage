4628f55ea0ae95474a5f9ff51505847aca318cea
Huda Ridwan <kerupuk.lovers@gmail.com>
2018-02-06 16:41:45 +0700

    Fix json format dummy data

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const app = express(); // define our app using express
 
-// const ListProxies = require('./proxy.json');
+const ListProxies = require('../proxy.json');
 
 
 
